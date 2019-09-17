53b2f051e81a6d818cf3af19289dac1e65bbc7ac
Andrew Browning <abrowning@faceco.net>
2019-09-17 07:23:28 -0700

    removed input filter
    changed some other things that were inconsequential.
    
    ABS light goes off, but ABS is super sensitive, suggesting that the period conversion is wrong.

--- ABS_converter/ABS_converter.ino
+++ ABS_converter/ABS_converter.ino
@@ -1,6 +1,6 @@
 unsigned long halfwave = 200;
 
-unsigned long period1 = 1000000;
+unsigned long period1 = 10000000;
 unsigned long periodflag1;
 unsigned long delayflag1;
 unsigned long signal1start;
@@ -10,7 +10,7 @@ unsigned long periodtimer1;
 unsigned long microsISRbuffer1;
 unsigned long period1buffer;
 
-unsigned long period2 = 1000000;
+unsigned long period2 = 10000000;
 unsigned long periodflag2;
 unsigned long delayflag2;
 unsigned long signal2start;
@@ -23,13 +23,13 @@ unsigned long period2buffer;
 unsigned long VSSfactor1;
 unsigned long VSSfactor2;
 unsigned long VSSperiodbuffer;
-unsigned long VSSperiod = 100000000;
+unsigned long VSSperiod = 10000000;
 unsigned long VSSperiodtimer;
 unsigned long VSSdelayflag;
 unsigned long VSSlightoff = 0;
 
-int complement1 = 97; // must add to exactly 100. int to avoid float math
-int complement2 = 3;
+int complement1 = 1; // must add to exactly 100. int to avoid float math
+int complement2 = 99;
 
 #define WSS1PIN1  3
 #define WSS1PIN2  4
@@ -41,12 +41,13 @@ int complement2 = 3;
 #define ERR9921   17
 #define OUT19921  18
 #define OUT29921  19
+#define ONBOARDLED 13
 
 
 
 const unsigned long inputwheelteeth = 96;
 const unsigned long outputwheelteeth = 44;
-const unsigned long VSSwheelteeth = 10; //this number is made up. Set to correct wheel count from transmission
+const unsigned long VSSwheelteeth = 5; //this number is made up. Set to correct wheel count from transmission
 
 
 bool signal1started = false;
@@ -67,8 +68,8 @@ bool serial2toggle = false;
 
 void setup() {
   // put your setup code here, to run once:
-//  Serial.begin(115200);
-//  Serial.println("start");
+  //  Serial.begin(115200);
+  //  Serial.println("start");
 
   //wheelspeed outputs use two output pins through a voltage divider for 0, 1.75, and 3v (00, 01, and 11)
   pinMode(WSS1PIN1, OUTPUT); //wheelspeed 1 output 1
@@ -83,12 +84,15 @@ void setup() {
   pinMode(OUT19921, INPUT); //Hall 1 signal from MAX9921
   pinMode(OUT29921, INPUT); //Hall 2 signal from MAX9921
 
+  pinMode(ONBOARDLED, OUTPUT); //onboard LED to display power
+  digitalWriteFast(ONBOARDLED, HIGH); //display power state
+
   delay(10); //give the max some time to get ready.
   digitalWriteFast(OE9921, HIGH); //enable max9921
   delay(10);
 
-  attachInterrupt(digitalPinToInterrupt(OUT19921), input1ISR, RISING);
-  attachInterrupt(digitalPinToInterrupt(OUT29921), input2ISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(OUT19921), input1ISR, FALLING);
+  attachInterrupt(digitalPinToInterrupt(OUT29921), input2ISR, FALLING );
 
   delayflag1 = micros(); //sets up initial conditions for WSS output waveform
   periodflag1 = delayflag1; //avoid second call to micros(), trickrepeated throughout
@@ -115,22 +119,26 @@ void loop() {
 
   //  Write to the serial buffer when the ISR has been accessed, then set up new waveform timings
   if (input1found) {
-//    if (serial1toggle) {
-//      Serial.println("1ping");
-//      serial1toggle = !serial1toggle;
-//    }
-//    else {
-//      Serial.println("1gnip");
-//      serial1toggle = !serial1toggle;
-//    }
+    //    if (serial1toggle) {
+    //      Serial.println("1ping");
+    //      serial1toggle = !serial1toggle;
+    //    }
+    //    else {
+    //      Serial.println("1gnip");
+    //      serial1toggle = !serial1toggle;
+    //    }
     period1buffer = microsISRbuffer1 - periodflag1;
     VSSfactor1 = period1buffer;
     period1buffer = period1buffer * inputwheelteeth;
     period1buffer = period1buffer / outputwheelteeth;
-    period1buffer = period1buffer * complement1;
-    period1 = period1 * complement2;
-    period1 = period1 + period1buffer;
-    period1 = period1 / 100;
+
+    //    period1buffer = period1buffer * complement1;
+    //    period1 = period1 * complement2;
+    //    period1 = period1 + period1buffer;
+    //    period1 = period1 / 100;
+
+    period1 = period1buffer; //removed filter
+
     periodflag1 = microsISRbuffer1;
     signal1send = period1 - halfwave;
     signal1end = period1;
@@ -140,22 +148,26 @@ void loop() {
   }
 
   if (input2found) {
-//    if (serial2toggle) {
-//      Serial.println("2ping");
-//      serial2toggle = !serial2toggle;
-//    }
-//    else {
-//      Serial.println("2gnip");
-//      serial2toggle = !serial2toggle;
-//    }
+    //    if (serial2toggle) {
+    //      Serial.println("2ping");
+    //      serial2toggle = !serial2toggle;
+    //    }
+    //    else {
+    //      Serial.println("2gnip");
+    //      serial2toggle = !serial2toggle;
+    //    }
     period2buffer = microsISRbuffer2 - periodflag2;
     VSSfactor2 = period2buffer;
     period2buffer = period2buffer * inputwheelteeth;
     period2buffer = period2buffer / outputwheelteeth;
-    period2buffer = period2buffer * complement1;
-    period2 = period2 * complement2;
-    period2 = period2 + period2buffer;
-    period2 = period2 / 100;
+
+    //    period2buffer = period2buffer * complement1;
+    //    period2 = period2 * complement2;
+    //    period2 = period2 + period2buffer;
+    //    period2 = period2 / 100;
+
+    period2 = period2buffer; //removed filter
+
     periodflag2 = microsISRbuffer2;
     signal2send = period2 - halfwave;
     signal2end = period2;
@@ -167,14 +179,14 @@ void loop() {
   //generate waveform for output 1
   if (periodtimer1 - delayflag1 >= signal1start) {
     if (!signal1started) {
-      digitalWriteFast(WSS1PIN1, HIGH);
-      digitalWriteFast(WSS1PIN2, HIGH);
+      digitalWriteFast(WSS1PIN1, LOW);
+      digitalWriteFast(WSS1PIN2, LOW);
       signal1started = true;
     }
     if (!signal1sent) {
       if (periodtimer1 - delayflag1 >= signal1send) {
-        digitalWriteFast(WSS1PIN1, LOW);
-        digitalWriteFast(WSS1PIN2, LOW);
+        digitalWriteFast(WSS1PIN1, HIGH);
+        digitalWriteFast(WSS1PIN2, HIGH);
         signal1sent = true;
         signal1ended = false;
       }
@@ -194,14 +206,14 @@ void loop() {
   //generate waveform for output 2
   if (periodtimer2 - delayflag2 >= signal2start) {
     if (!signal2started) {
-      digitalWriteFast(WSS2PIN1, HIGH);
-      digitalWriteFast(WSS2PIN2, HIGH);
+      digitalWriteFast(WSS2PIN1, LOW);
+      digitalWriteFast(WSS2PIN2, LOW);
       signal2started = true;
     }
     if (!signal2sent) {
       if (periodtimer2 - delayflag2 >= signal2send) {
-        digitalWriteFast(WSS2PIN1, LOW);
-        digitalWriteFast(WSS2PIN2, LOW);
+        digitalWriteFast(WSS2PIN1, HIGH);
+        digitalWriteFast(WSS2PIN2, HIGH);
         signal2sent = true;
         signal2ended = false;
       }
@@ -219,7 +231,7 @@ void loop() {
   }
 
   //generate waveform for VSS
-  if (VSSlightoff < 1000) {
+  if (VSSlightoff < 1000000) {
     if (VSSperiodtimer - VSSdelayflag >= VSSperiod) {
       if (VSStoggle) {
         digitalWriteFast(VSSPIN, HIGH);
@@ -250,8 +262,12 @@ void updateVSS() {
   VSSperiodbuffer = VSSperiodbuffer / 2;  //take the average of the two wheel speed sensors
   VSSperiodbuffer = VSSperiodbuffer * inputwheelteeth;
   VSSperiodbuffer = VSSperiodbuffer / VSSwheelteeth;
-  VSSperiodbuffer = VSSperiodbuffer * complement1;
-  VSSperiod = VSSperiod * complement2;
-  VSSperiod = VSSperiod + VSSperiodbuffer;
-  VSSperiod = VSSperiod / 200; //divide by 200 instead of 100 because we'll be toggling VSS on or off every half period
+
+  //  VSSperiodbuffer = VSSperiodbuffer * complement1;
+  //  VSSperiod = VSSperiod * complement2;
+  //  VSSperiod = VSSperiod + VSSperiodbuffer;
+  //  VSSperiod = VSSperiod / 200; //divide by 200 instead of 100 because we'll be toggling VSS on or off every half period
+
+  VSSperiod = VSSperiodbuffer/2;
+
 }
